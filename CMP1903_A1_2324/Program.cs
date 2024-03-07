8e98c198a7e65ab79d0425eb6813a6c137b96980
Kuba <Kuba.skrzypczyski@gmail.com>
2024-03-07 01:30:40 +0000

    Improved code from feedback
    added comments

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -12,14 +12,19 @@ namespace CMP1903_A1_2324
         {
             try
             {
+                // Creates a game object
                 Game game = new Game();
 
+                // Calls the Rolls method in the game class
                 game.Rolls();
 
+                // Creates a testing object
                 Testing testing = new Testing();
 
+                // Calls the TestGame method in the testing class
                 testing.TestGame();
             }
+            // Catch exception to handle any errors
             catch (Exception)
             {
                 Console.WriteLine("An error occured in the main method");
